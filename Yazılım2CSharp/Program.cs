38bb5ce4f4b7c263c607b789914b4602ca0ae21e
emirgorkemozdemir <60661125+emirgorkemozdemir@users.noreply.github.com>
2024-12-26 19:32:33 +0300

    Add project files.

---
+++ Yazılım2CSharp/Program.cs
@@ -0,0 +1,33 @@
+﻿// Yorum satırı
+/*
+ * Çoklu yorum satırı
+ */
+
+int num1 = 5;
+
+// int => tam sayı.
+// Int16 en küçük integer short
+// Int32 normal int
+// Int64 long büyük int
+// Int128 en büyük
+
+// double => ondalıklı sayı
+// float => ondalıklı sayı
+// decimal => ondalıklı sayı (money)
+float num2 = 5.5F;
+decimal num3 = 6.4M;
+
+// string => uzun karakterler ""
+// char => tek karaker ''
+char karakter = 'E';
+string cumle = "asdhfasdhf asdfasdhfa";
+
+// bool => bu degişkenler mantık degişkenidir, yani bir sonuç tutar
+// bir durumu degerlendirir, dogruysa true gelir yanlışsa false gelir.
+
+// var ile degişken oluşturmak
+var deneme = 'E';
+
+// Const olarak açılan degerler degiştirilemez
+const int num4 = 6;
+// num4 = 5;
