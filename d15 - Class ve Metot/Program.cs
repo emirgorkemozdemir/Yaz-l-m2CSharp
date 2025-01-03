b9b3277d7e4970083fb3b8f776bcb5f1807700cc
emirgorkemozdemir <60661125+emirgorkemozdemir@users.noreply.github.com>
2025-01-03 18:41:19 +0300

    ccc

--- d15 - Class ve Metot/Program.cs
+++ d15 - Class ve Metot/Program.cs
@@ -9,4 +9,20 @@
 
 //Console.WriteLine(Solutions.nothesapla(60, 90));
 
-Console.WriteLine(Solutions.harfarttır("emir",5));
+//Console.WriteLine(Solutions.harfarttır("emir",5));
+
+//List<int> sayılar = new List<int>();
+//sayılar.Add(80);
+//sayılar.Add(3);
+//sayılar.Add(90);
+//Console.WriteLine(Solutions.max_fark(sayılar));
+
+//Console.WriteLine(Solutions.asal_mi(36));
+
+//Console.WriteLine(Solutions.ortalama_pozitif_mi());
+//Console.Beep();
+
+List<string> ulkeler = new List<string>() { "Turkey", "Turkey", "Turkey", "Germany", "Spain" };
+
+//Console.WriteLine(Solutions.palindrom_mu("Ada"));
+Solutions.tekrar_edeni_sil(ulkeler);
--- d15 - Class ve Metot/Solutions.cs
+++ d15 - Class ve Metot/Solutions.cs
@@ -83,5 +83,127 @@ namespace d15___Class_ve_Metot
 
             return sonuc;
         }
+
+        
+        // Bir fonksiyon açın, bu fonksiyona bir List<int> girilsin.
+        // Bu listedeki en büyük sayı farkını bulun.
+        // {80,3,90} => Döngüyle listedekileri tek tek alıp maximum
+        // farkı buldurmanız lazım. Her ögeyi tek tek digerinden çıkarın.
+        // İç içe döngü var.
+        public static int max_fark(List<int> sayılar)
+        {
+            int max = 0;
+            foreach (var tur1 in sayılar)
+            {
+                foreach (var tur2 in sayılar)
+                {
+                    if (tur1-tur2 > max)
+                    {
+                        max = tur1 - tur2;
+                    }
+                }
+            }
+            return max;
+        }
+
+        // Bir fonksiyon açın, bu fonksiyona bir adet sayı girilsin.
+        // Sayının asal olup olmadıgını döndürün.
+        // Asalsa true, degilse false.
+
+        public static bool asal_mi(int sayı)
+        {
+            for (int i = 2; i < sayı; i++)
+            {
+                if (sayı%i==0)
+                {
+                    return false;
+                }
+            }
+            
+            return true;
+        }
+
+        // 10 kere sayı girdirin. Bu sayıların ortalamasını bulun.
+        // Sayıların ortalaması pozitif ise true, degilse false döndürün
+        public static bool ortalama_pozitif_mi()
+        {
+            int sonuc = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Sayı giriniz : ");
+                int sayı = Convert.ToInt32(Console.ReadLine());
+                sonuc += sayı;
+            }
+
+            sonuc = sonuc / 10;
+            if (sonuc >0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        // Bir metot açın, metoda bir string kelime girilsin.
+        // Kelimenin tersiyle düzü aynı mı diye kontrol edin.
+        // Örnegin :
+        // Kelime mom ise tersiyle düzü aynıdır.
+        // Tersiyle düzü aynıysa true, degilse false.
+        public static bool palindrom_mu(string kelime)
+        {
+            string kelimenin_tersi = "";
+            for (int i = kelime.Length-1; i >= 0; i--)
+            {
+                kelimenin_tersi += kelime[i];
+            }
+
+            if (kelime.ToLower()==kelimenin_tersi.ToLower())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        // Bir tane metot açın. 
+        // Parametre olarak List<string> ulkeler alsın.
+        // Bu listenin içerisinde birden fazla aynı degerden varsa sildiren
+        // metodu oluşturun.
+        // {"Turkey","Turkey","Germany","Spain"}
+        // {"Turkey","Turkey","Germany","Spain"}
+        // Eger bir deger silindiyse ekrana deger listeden silindi yazsın.
+        // iki döngü var!!!
+
+        public static void tekrar_edeni_sil(List<string> ulkeler)
+        {
+            // {"Turkey","Turkey","Germany","Spain"}
+            for(int i=0;i<ulkeler.Count;i++)
+            {
+                int sayac = 0;
+                for (int j = 0; j < ulkeler.Count; j++)
+                {
+                    if (ulkeler[i] == ulkeler[j])
+                    {
+                        sayac++;
+                    } 
+                }
+
+                if (sayac>=2)
+                {
+                    ulkeler.Remove(ulkeler[i]);
+                }
+            }
+
+            foreach (var ulke in ulkeler)
+            {
+                Console.WriteLine(ulke);
+            }
+        }
     }
 }
